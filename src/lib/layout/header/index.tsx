91aef4840dd193616608fdb0ae5d48b4654c9703
BernardoFofg-NE <bernardofofg@gmail.com>
2024-02-15 12:08:23 -0300

    fix boilerplate

--- src/lib/layout/header/index.tsx
+++ src/lib/layout/header/index.tsx
@@ -2,7 +2,7 @@ import { Flex, Text } from '@chakra-ui/react';
 
 export const Header = () => {
   return (
-    <Flex w="100%" h="10%" bg="background.secondary">
+    <Flex w="100%" h="6rem" bg="background.secondary">
       <Text>header</Text>
     </Flex>
   );
--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,21 +1,12 @@
-import {
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Input,
-  InputGroup,
-  Stack,
-} from '@chakra-ui/react';
-import { useState } from 'react';
+import { Flex } from '@chakra-ui/react';
+
 import { Layout } from '~/lib/layout';
-import axios from 'axios';
 
 export const Home = () => {
   return (
     <Layout>
-      <Flex justify="center" align="center" w="100vw" h="100vh">
-        <iframe width="100%" height="100%" src="https://headsbet.com" />
+      <Flex justify="center" align="center" w="100%" h="100vh">
+        Você vai começar aqui!
       </Flex>
     </Layout>
   );
